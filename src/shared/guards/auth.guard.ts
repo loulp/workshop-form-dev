a0dcbf390df520c469f1eab09f27d82f87aac4b8
loulp <louislpogam@orange.fr>
2022-06-28 15:58:09 +0200

    fix logged status

--- src/shared/guards/auth.guard.ts
+++ src/shared/guards/auth.guard.ts
@@ -19,7 +19,6 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    
     const isAuth = (paths: Array<any>) => paths.some((path) => path === route.routeConfig?.path);
 
     if (!this.isSignedIn && !isAuth(['login'])) {
